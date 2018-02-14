4342ff1199e9be6cb104d308efcda6d81c46af1a
Samart Nuntakhwang <samart@ipassion.co.th>
2018-02-14 13:59:48 +0700

    Init Commit

---
+++ SQL Script/Validate TLP Data/Validate TLP Premium.sql
@@ -0,0 +1,86 @@
+--Total Premium
+SELECT --count(0) 
+info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.totpremium
+FROM tlp.splitpremiumamount prem  
+join tlp.customerinfo info 
+	on prem.grouppolicyno = info.policyno 
+	and prem.customercode = info.customercode 
+	and prem.productid = info.productcode
+where prem.totpremium = '0.00' and customerstatus in (select customerstatus from lookup.tlppolicystatus)
+order by info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.totpremium
+;
+
+--Life Premium
+SELECT --count(0) 
+info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.lifepremium, prem.deathpremium
+FROM tlp.splitpremiumamount prem  
+join tlp.customerinfo info 
+	on prem.grouppolicyno = info.policyno 
+	and prem.customercode = info.customercode 
+	and prem.productid = info.productcode
+where prem.lifepremium = '0.00' and prem.deathpremium = '0.00' 
+and customerstatus in (select customerstatus from lookup.tlppolicystatus)
+order by info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.lifepremium
+;
+
+--TPD Premium
+SELECT --count(0) 
+info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.tpdpremium
+FROM tlp.splitpremiumamount prem  
+join tlp.customerinfo info 
+	on prem.grouppolicyno = info.policyno 
+	and prem.customercode = info.customercode 
+	and prem.productid = info.productcode
+join (select * from lookup.smi where coverage = 'TPD') b 
+	on trim (prem.grouppolicyno) = trim (b.tlpplancode)
+	and trim (info.policyno) = trim (b.tlpplancode)
+where prem.tpdpremium = '0.00' 
+--and info.policyno not in ('00019004', '00019005', '00019006')
+and customerstatus in (select customerstatus from lookup.tlppolicystatus)
+order by info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.tpdpremium
+;
+
+--TD Premium
+SELECT count(0) 
+--info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.tdpremium
+FROM tlp.splitpremiumamount prem  
+join tlp.customerinfo info 
+	on prem.grouppolicyno = info.policyno 
+	and prem.customercode = info.customercode 
+	and prem.productid = info.productcode
+join (select * from lookup.smi where coverage = 'TD') b 
+	on trim (prem.grouppolicyno) = trim (b.tlpplancode)
+	and trim (info.policyno) = trim (b.tlpplancode)
+where prem.tdpremium = '0.00' and customerstatus in (select customerstatus from lookup.tlppolicystatus)
+--order by info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.tdpremium
+;
+
+--ADD Premium
+SELECT --count(0) 
+info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.addpremium
+FROM tlp.splitpremiumamount prem  
+join tlp.customerinfo info 
+	on prem.grouppolicyno = info.policyno 
+	and prem.customercode = info.customercode 
+	and prem.productid = info.productcode
+join (select * from lookup.smi where coverage = 'ADD') b 
+	on trim (prem.grouppolicyno) = trim (b.tlpplancode)
+	and trim (info.policyno) = trim (b.tlpplancode)
+where prem.addpremium = '0.00' and customerstatus in (select customerstatus from lookup.tlppolicystatus)
+order by info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.addpremium
+;
+
+--AME Premium
+SELECT count(0) 
+--info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.mepremium
+FROM tlp.splitpremiumamount prem  
+join tlp.customerinfo info 
+	on prem.grouppolicyno = info.policyno 
+	and prem.customercode = info.customercode 
+	and prem.productid = info.productcode
+join (select * from lookup.smi where coverage = 'AME') b 
+	on trim (prem.grouppolicyno) = trim (b.tlpplancode)
+	and trim (info.policyno) = trim (b.tlpplancode)
+where prem.mepremium = '0.00' and customerstatus in (select customerstatus from lookup.tlppolicystatus)
+--order by info.productcode, info.policyno, info.certificateno, info.customercode, info.customerstatus, prem.mepremium
+;
---
+++ SQL Script/Validate TLP Data/Validate TLP SA.sql
@@ -0,0 +1,125 @@
+--Validate Life Benefit
+select --count (0) 
+a.policyno, info.certificateno, info.customercode, info.sa as info_sa, a.sa_life, b.benefit,  info.customerstatus,
+ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100)) as validate,
+ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100))  - to_number(a.sa_life, '99999999999.99') as diff
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+inner join (select * from lookup.smi where coverage = 'LIFE') b 
+	on trim (a.policyno) = trim (b.tlpplancode)
+	and trim(a.policyno) = trim(info.policyno) 
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+--and abs (ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100))  - to_number(a.sa_life, '99999999999.99')) <> 1
+and to_number(a.sa_life, '99999999999.99') <> ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100));
+
+
+--Validate Rider TPD
+select --count (0) 
+a.policyno, info.certificateno, a.customercode, info.sa as info_sa, a.sa_tpd, b.benefit,  
+ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100)) as validate,
+ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100)) - to_number(a.sa_tpd, '99999999999.99') as diff
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+inner join (select * from lookup.smi where coverage = 'TPD') b 
+	on trim (a.policyno) = trim (b.tlpplancode) 
+	and trim(info.policyno) = trim(b.tlpplancode) 
+where info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+--and abs(ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100)) - to_number(a.sa_tpd, '99999999999.99')) <> 1
+and to_number(a.sa_tpd, '99999999999.99') <> ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100));
+
+--Validate Rider TD
+select count (0)
+/*
+a.policyno, a.customercode, info.sa as info_sa, a.sa as sa_sa, a.sa_td, 
+b.benefit, to_number(info.durationinsurance, '99') as durationinsurance,
+ceiling ((to_number(info.sa, '99999999999.99') * (b.benefit/100)) / (to_number(info.durationinsurance, '99') * 12 )) as validate,
+ceiling ((to_number(info.sa, '99999999999.99') * (b.benefit/100)) / (to_number(info.durationinsurance, '99') * 12 )) - 
+to_number(a.sa_td, '99999999999.99') as diff*/
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+inner join (select * from lookup.smi where coverage = 'TD') b 
+	on trim (a.policyno) = trim (b.tlpplancode)
+	and trim(info.policyno) = trim (b.tlpplancode) 
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+and abs(ceiling ((to_number(info.sa, '99999999999.99') * (b.benefit/100)) / (to_number(info.durationinsurance, '99') * 12 )) - to_number(a.sa_td, '99999999999.99')) <> 1
+and to_number(a.sa_td, '99999999999.99') <> ceiling ((to_number(info.sa, '99999999999.99') * (b.benefit/100)) / (to_number(info.durationinsurance, '99') * 12 )) ;
+
+--Validate Rider ADD-ML46
+select --count (0) 
+a.policyno, a.customercode, info.sa as info_sa, a.sa as sa_sa, a.sa_add, b.benefit,  
+b.benefit as validate, to_number(info.sa, '99999999999.99') - b.benefit as diff
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+inner join (select * from lookup.smi where coverage = 'ADD' and tlipolicy = 'ML46') b 
+	on trim (a.policyno) = trim (b.tlpplancode)
+	and trim(info.policyno) = trim(b.tlpplancode) 
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+and to_number(a.sa_add, '99999999999.99') <> b.benefit;
+
+--Validate Rider ADD-LE31
+select --count (0) 
+a.policyno, a.customercode, info.sa as info_sa, a.sa as sa_sa, a.sa_add, b.benefit,  
+b.benefit as validate, to_number(info.sa, '99999999999.99') - b.benefit as diff
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+inner join (select * from lookup.smi where coverage = 'ADD' and tlipolicy = 'LE31') b 
+	on trim (a.policyno) = trim (b.tlpplancode)
+	and trim(info.policyno) = trim(b.tlpplancode) 
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+and to_number(a.sa_add, '99999999999.99') <> b.benefit;
+
+--Validate Rider ADD not ML46, LE31
+select --count (0) 
+a.policyno, info.certificateno, a.customercode, info.sa as info_sa, a.sa_add, b.benefit,  
+ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100)) as validate,
+ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100)) - to_number(a.sa_add, '99999999999.99') as diff
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+inner join (select * from lookup.smi where coverage = 'ADD' and tlipolicy not in ('ML46', 'LE31') ) b 
+	on trim (a.policyno) = trim (b.tlpplancode)
+	and trim(info.policyno) = trim(b.tlpplancode) 
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+--and abs (ceiling (to_number(a.sa, '99999999999.99') * (b.benefit/100)) - to_number(a.sa_add, '99999999999.99')) <> 1
+and to_number(a.sa_add, '99999999999.99') <> ceiling (to_number(info.sa, '99999999999.99') * (b.benefit/100));
+
+--Validate Rider AME
+select --count (0) 
+a.policyno, info.certificateno, a.customercode, info.sa as info_sa, a.sa_me, b.benefit,  
+b.benefit as validate, to_number(info.sa, '99999999999.99') - b.benefit as diff
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode 
+inner join (select * from lookup.smi where coverage = 'AME') b 
+	on trim (a.policyno) = trim (b.tlpplancode)
+	and trim(info.policyno) = trim(b.tlpplancode) 
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+and to_number(a.sa_me, '99999999999.99') <> b.benefit;
+
+--Check customerinfo.sa equals to customersa.sa or not ?
+select info.customerstatus, info.sa, a.sa 
+from tlp.customerinfo info 
+join tlp.customersa a 
+	on a.policyno = info.policyno 
+	and a.customercode = info.customercode
+where 1=1 
+and info.customerstatus in (select customerstatus from lookup.tlppolicystatus) 
+and to_number(info.sa, '99999999999.99') <> to_number(a.sa, '99999999999.99');

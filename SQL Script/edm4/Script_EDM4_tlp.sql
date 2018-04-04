7a70fa8a8d8dc082cc9fab932c10f16c4027cfe8
DESKTOP-5JUIEJ3\iPassion03 <sumalee.k>
2018-04-04 15:36:33 +0700

    Add New Table Script for tlp

--- SQL Script/edm4/Script_EDM4_tlp.sql
+++ SQL Script/edm4/Script_EDM4_tlp.sql
@@ -1087,17 +1087,6 @@ WITH (
 	OIDS=FALSE
 ) ;	
 
-CREATE TABLE tlp.productuobbranchprovince(
-	provinceid text NULL,
-	pcode text NULL,
-	province text NULL,
-	flaguobbranch text NULL,
-	region text NULL,
-	branchcodechqpay text NULL
-) 
-WITH (
-	OIDS=FALSE
-) ;	
 
 CREATE TABLE tlp.productwhtratesetup(
 	whtaxtype text NULL,
@@ -1503,3 +1492,502 @@ CREATE TABLE tlp.maturityhistory(
 WITH (
 	OIDS=FALSE
 ) ;	
+
+
+CREATE TABLE tlp.customerinfo(
+	productname text NULL,
+	policyno text NULL,
+	productcode text NULL,
+	certificateno text NULL,
+	saledate text NULL,
+	branchname text NULL,
+	customercode text NULL,
+	accountno text NULL,
+	loanno text NULL,
+	totalofloan text NULL,
+	sa text NULL,
+	prem text NULL,
+	durationinsurance text NULL,
+	insurancestatus text NULL,
+	effectivedate text NULL,
+	expireddate text NULL,
+	customerstatus text NULL,
+	canceldate text NULL,
+	cancelreason text NULL,
+	canceldescription text NULL,
+	channel text NULL,
+	prefix text NULL,
+	nameth text NULL,
+	surnameth text NULL,
+	birthdate text NULL,
+	age text NULL, 
+	weight text NULL,
+	height text NULL,
+	gender text NULL,
+	status text NULL,
+	nationid text NULL,
+	creditcardno text NULL,
+	expiredcreditcard text NULL,
+	address1 text NULL,
+	address2 text NULL,
+	addressoffice1 text NULL,
+	addressoffice2 text NULL,
+	nameofbenefit1 text NULL,
+	nameofbenefit2 text NULL,
+	nameofbenefit3 text NULL,
+	nameofbenefit4 text NULL,
+	relationship1 text NULL,
+	relationship2 text NULL,
+	relationship3 text NULL,
+	relationship4 text NULL,
+	hometel text NULL,
+	mobiletel text NULL,
+	officetel text NULL,
+	officercodesale text NULL,
+	salenameth text NULL,
+	managercode text NULL,
+	dateprintcert text NULL,
+	postbackdate text NULL,
+	cancelletterno text NULL,
+	printcancelletterdate text NULL,
+	partnercode text NULL,
+	partnername text NULL,
+	databasesource text NULL,
+	refname text NULL,
+	refnationid text NULL,
+	refcreditcardno text NULL,
+	facility text NULL,
+	uwdescription text NULL,
+	uwstatus text NULL,
+	tmksaledate text NULL,
+	coverage text NULL,
+	paidtodate text NULL,
+	extrarate text NULL,
+	voicetag text NULL,
+	deathdate text NULL,
+	customerloanapprovedate text NULL,
+	lob text NULL,
+	canceltransactiondate text NULL,
+	oldaccountno text NULL,
+	customeroccupationtext text NULL,
+	loaninstallment1stdue text NULL,
+	customercontactofficepay text NULL,
+	percentofbenefitciary1 text NULL,
+	percentofbenefitciary2 text NULL,
+	percentofbenefitciary3 text NULL,
+	percentofbenefitciary4 text NULL,
+	downplaneffectivedate text NULL,
+	date_sosover text NULL,
+	customerduration text NULL,
+	med text NULL,
+	customernation text NULL,
+	endorsedate text NULL,
+	informdate1 text NULL,
+	paydate1 text NULL,
+	amount1 text NULL,
+	informdate2 text NULL,
+	paydate2 text NULL,
+	amount2 text NULL,
+	deaddate text NULL,
+	lastpaydate text NULL,
+	lastreceiptno text NULL,
+	mode text NULL,
+	lastpayperiod text NULL
+) 
+WITH (
+	OIDS=FALSE
+) ;	
+
+
+CREATE TABLE tlp.customersa(
+	customercode text NULL,
+	policyno text NULL,
+	sa_lifetpd text NULL,
+	sa_life text NULL,
+	sa_tpd text NULL,
+	sa_td text NULL,
+	sa_add text NULL,
+	sa_adb text NULL,
+	sa_ha text NULL,
+	sa_hb text NULL,
+	sa_me text NULL,
+	sa_pa text NULL,
+	sa_ph text NULL,
+	sa_ci text NULL,
+	flaglife text NULL,
+	flaglifetpd text NULL,
+	ia text NULL,
+	sa text NULL,
+	transdate text NULL,
+	mi_amount text NULL,
+	expiry_life text NULL,
+	expiry_tpd text NULL, 
+	expiry_td text NULL,
+	expiry_add text NULL,
+	expiry_adb text NULL,
+	expiry_ha text NULL,
+	expiry_hb text NULL,
+	expiry_me text NULL,
+	expiry_pa text NULL,
+	expiry_ph text NULL,
+	expiry_ci text NULL,
+	sa_ma text NULL,
+	expiry_ma text NULL,
+	sa_mc text NULL,
+	expiry_mc text NULL
+) 
+WITH (
+	OIDS=FALSE
+) ;	
+
+CREATE TABLE tlp.payment(
+	partnercode text NULL,
+	policyno text NULL,
+	productcode text NULL,
+	customercode text NULL,
+	duedate text NULL,
+	senddate text NULL,
+	ob text NULL,
+	paymentdate text NULL,
+	premiumamt text NULL,
+	adjustpremium text NULL,
+	typeofpayment text NULL,
+	accno text NULL,
+	refund text NULL,
+	dateofrefund text NULL,
+	invoiceno text NULL,
+	monthenddate text NULL,
+	pay_date text NULL,
+	refund_amount text NULL,
+	collecteddate text NULL,
+	mode text NULL,
+	payperiod text NULL,
+	receiptno text NULL
+) 
+WITH (
+	OIDS=FALSE
+) ;	
+
+CREATE TABLE tlp.splitpremiumamount(
+	productid text NULL,
+	grouppolicyno text NULL,
+	customercode text NULL,
+	totpremium text NULL,
+	lifepremium text NULL,
+	deathpremium text NULL,
+	tpdpremium text NULL,
+	ttdpremium text NULL,
+	tdpremium text NULL,
+	papremium text NULL,
+	adbpremium text NULL,
+	addpremium text NULL,
+	rccpremium text NULL,
+	mapremium text NULL,
+	mcpremium text NULL,
+	mepremium text NULL,
+	hbpremium text NULL,
+	hapremium text NULL,
+	hspremium text NULL,
+	ddpremium text NULL,
+	lifetpdpremium text NULL,
+	receiptno text NULL
+) 
+WITH (
+	OIDS=FALSE
+) ;	
+
+CREATE TABLE tlp.customerballoon (
+	policyno text NULL,
+	certno text NULL,
+	policynoballoon text NULL,
+	certnobolloon text NULL,
+	loancontractno text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+
+CREATE TABLE tlp.masproduct (
+	productseq text NULL,
+	policyno text NULL,
+	productcode text NULL,
+	productplan text NULL,
+	productnameen text NULL,
+	productnameth text NULL,
+	productgroup text NULL,
+	productstatus text NULL,
+	partnerseq text NULL,
+	producttype text NULL,
+	partnerlettername text NULL,
+	percent_life text NULL, 
+	percent_tpd text NULL,
+	percent_add text NULL,
+	percent_adb text NULL,
+	percent_pa text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlp.masproductcoverage(
+	productcoverageseq text NULL, 
+	productcoveragepaymentmode text NULL, 
+	productcoveragesa text NULL, 
+	productcoverageamount text NULL, 
+	productcoveragewaitingperiod text NULL, 
+	productcoverageexcludeperiod text NULL, 
+	productcoveragedesc text NULL, 
+	productseq text NULL, 
+	riskcoverageseq text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlp.masproductcoverageclaim(
+	productcoverageclaimseq text NULL, 
+	productcoverageclaimonetime text NULL, 
+	productcoverageseq text NULL, 
+	claimtypeseq text NULL, 
+	risktypeseq text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlp.claimregisterheader(
+	registerseq text NULL, 
+	regcustomerbranchname text NULL, 
+	regcustomercode text NULL, 
+	regcustomercertificateno text NULL, 
+	regcustomerloanno text NULL, 
+	regcustomeracctno text NULL, 
+	regcustomeridcard text NULL, 
+	regcustomerprefix text NULL, 
+	regcustomername text NULL, 
+	regcustomerfname text NULL, 
+	regcustomerlname text NULL, 
+	regcustomerdob text NULL, 
+	regcustomerage text NULL, 
+	regcustomergender text NULL, 
+	regcustomerstatus text NULL, 
+	regcustomercardtype text NULL, 
+	regcustomercardno text NULL, 
+	regcustomercardlimit text NULL, 
+	regcustomercardinstl text NULL, 
+	regcustomeraddr01 text NULL, 
+	regcustomeraddr02 text NULL, 
+	regcustomeraddr03 text NULL, 
+	regcustomerprovince text NULL, 
+	regcustomerzipcode text NULL, 
+	regcustomertelephone text NULL, 
+	regcustomerloanamount text NULL, 
+	regcustomersumassured text NULL, 
+	regcustomerpremium text NULL, 
+	regcustomerdurationinsurance text NULL, 
+	regcustomermodeofpayment text NULL, 
+	regcustomersaledate text NULL, 
+	regcustomeruwstatuscode text NULL, 
+	regcustomeruwstatusdesc text NULL, 
+	regcustomercontractdate text NULL, 
+	regcustomerissuedate text NULL, 
+	regcustomereffectivedate text NULL, 
+	regcustomerexpiredate text NULL, 
+	regcustomervehreceivedate text NULL, 
+	regcustomerfirstpaymentdate text NULL, 
+	regcustomerlastpaymentdate text NULL, 
+	regcustomerpaidtodate text NULL, 
+	regcustomerpolicystatuscode text NULL, 
+	regcustomerpolicystatusdesc text NULL, 
+	regcustomercanceldate text NULL, 
+	regcustomercancelreason text NULL, 
+	regcustomercanceldescription text NULL, 
+	regcustomercancelletterno text NULL, 
+	claimno text NULL, 
+	productseq text NULL,  
+	datatype text NULL, 
+	regcustomerchannel text NULL, 
+	regcustomerrefid text NULL, 
+	age text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlp.claimregisterdetail(
+	regdtlseq text NULL, 
+	regdtlregisterno text NULL, 
+	regdtlclaimrunno text NULL, 
+	regdtlclaimno text NULL, 
+	regdtlletterrunno text NULL, 
+	regdtlletterno text NULL, 
+	regdtlclaimgroup text NULL, 
+	regdtlclaimcausecode text NULL, 
+	regdtlclaimcauseen text NULL, 
+	regdtlclaimcauseth text NULL,  
+	regdtldateofmedicalreceive text NULL, 
+	regdtldateofmedicalreport text NULL, 
+	regdtldateofcompletedoc text NULL, 
+	regdtlhospitalcode text NULL, 
+	regdtlhospitalname text NULL, 
+	regdtlpatientdeptcode text NULL, 
+	regdtlpatientdept text NULL, 
+	regdtldateofopd text NULL, 
+	regdtldateofaccident text NULL, 
+	regdtldateofdeath text NULL, 
+	regdtldateofadmission text NULL, 
+	regdtldateofdischarge text NULL, 
+	regdtldateofoccurred text NULL, 
+	regdtldurationfrom text NULL, 
+	regdtldurationto text NULL, 
+	regdtldurationdays text NULL, 
+	regdtlestimateclaim text NULL, 
+	regdtlclaimexpense text NULL, 
+	regdtlcoinsurancecode text NULL, 
+	regdtldeclinestatus text NULL, 
+	regdtltotalclaim text NULL, 
+	regdtltotalttdpercentage text NULL, 
+	regdtltotalttdamount text NULL, 
+	regdtltotaltpdpercentage text NULL, 
+	regdtltotaltpdamount text NULL, 
+	regdtladjustcode text NULL, 
+	regdtladjustpayment text NULL, 
+	regdtlinterest text NULL, 
+	regdtltotalamount text NULL, 
+	regdtltotalexgratiaamount text NULL, 
+	regdtlvoidamount text NULL, 
+	regdtlsavedenyamount text NULL, 
+	regdtlpartialpayment text NULL,  
+	regdtlchequeno text NULL, 
+	regdtlpaidtopartner text NULL, 
+	regdtlpaidtobeneficiary text NULL, 
+	regdtlclaimfollowup text NULL, 
+	regdtlclaimremark text NULL, 
+	regdtlclaimflag text NULL, 
+	regdtlregisterdate text NULL, 
+	regdtlregister text NULL, 
+	regdtlapprovestatus text NULL, 
+	regdtlapprovedate text NULL, 
+	regdtlapproval text NULL,  
+	regdtlpolicystatus text NULL,  
+	regdtlhbcflag text NULL, 
+	registerseq text NULL, 
+	claimtypeseq text NULL, 
+	coinsuranceseq text NULL, 
+	datatype text NULL, 
+	payment_id text NULL, 
+	sysuseclaim text NULL, 
+	regdtldblindemnity text NULL, 
+	regdtlclaimnoteapprove text NULL, 
+	regdtlcleanstatus text NULL, 
+	regdtlnote text NULL, 
+	regdtlcaseno text NULL, 
+	regdtlfaxclaim text NULL, 
+	regdtlbatchno text NULL,  
+	regdtlicdten1 text NULL, 
+	regdtlicdteneng1 text NULL, 
+	regdtlicdtenth1 text NULL, 
+	regdtlicdten2 text NULL,  
+	regdtlicdteneng2 text NULL, 
+	regdtlicdtenth2 text NULL, 
+	regdtlicdten3 text NULL, 
+	regdtlicdteneng3 text NULL, 
+	regdtlicdtenth3 text NULL, 
+	regdtltpaclaimtype text NULL, 
+	regdtllettermemocreatedate text NULL, 
+	regdtllettermemouser text NULL, 
+	regdtllettermemostatus text NULL, 
+	regdtllos text NULL, 
+	regdtlbenefitday text NULL, 
+	regdtlclonestatus text NULL, 
+	regdtlseqtpa text NULL, 
+	regdtlhismaprpt text NULL, 
+	regdtlclaimstatus text NULL, 
+	regdtlletterdate text NULL, 
+	regdtlletterpendingdate text NULL, 
+	regdtlletterdelaydate text NULL, 
+	regdtlletterreminddate text NULL, 
+	regdtlletterclosedate text NULL, 
+	regdtlletterpaymentdate text NULL, 
+	regdtllettervoiddate text NULL, 
+	regdtlletterdenydate text NULL, 
+	regdtlcalsa text NULL, 
+	regdtlestimateclaimamt text NULL, 
+	regdtlperproposal text NULL, 
+	claimtypesubseq text NULL,  
+	claimtypesubcode text NULL, 
+	regdtlclaimcausecancel text NULL, 
+	paytype text NULL, 
+	partnercode text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+
+CREATE TABLE tlp.claimpaymentdetail(
+	paymentdt_id text NULL,
+	payment_id text NULL,
+	paymenttype text NULL,
+	payeename text NULL,
+	pamentamt text NULL,
+	method text NULL,
+	relation text NULL,
+	receipt_name text NULL,
+	address1 text NULL,
+	address2 text NULL,
+	tumbon text NULL,
+	amphor text NULL,
+	province text NULL,
+	zipcode text NULL,
+	claimbenefit text NULL,
+	bankid text NULL,
+	bankname text NULL,
+	accno text NULL,
+	returncheque text NULL,
+	returndate text NULL,
+	payee_no text NULL,
+	createdate text NULL,
+	createuser text NULL,
+	updatedate text NULL,
+	updateuser text NULL, 
+	printref text NULL,
+	printref_runno text NULL,
+	chequeno text NULL,
+	receivechequedate text NULL,
+	remarkpayment text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+
+CREATE TABLE tlp.claimpayment(
+	payment_id text NULL,
+	registerseq text NULL,
+	claimno text NULL,
+	printdate text NULL,
+	requestby text NULL,
+	approveby1 text NULL,
+	approveby2 text NULL,
+	authorizedby1 text NULL,
+	authorizedby2 text NULL,
+	totalamt text NULL,
+	paid_date text NULL,
+	createdate text NULL,
+	createuser text NULL,
+	updateuser text NULL,
+	updatedate text NULL,
+	sysprintstatus text NULL,
+	canceldate text NULL,
+	lettertype text NULL,
+	reqdate text NULL,
+	financedate text NULL,
+	respdate text NULL,
+	deadlinedate text NULL,
+	payrate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;

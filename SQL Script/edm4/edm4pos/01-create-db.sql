19ef9555d8cbb3761465eba4dd479af6e81c820c
Samart Nuntakhwang <samart@ipassion.co.th>
2018-05-24 15:59:42 +0700

    Remove field outputresult.

--- SQL Script/edm4/edm4pos/01-create-db.sql
+++ SQL Script/edm4/edm4pos/01-create-db.sql
@@ -37,7 +37,6 @@ CREATE TABLE tlpktc.billingdetail (
 	uttxreferno2 text NULL,
 	uttxtrxntype text NULL,
 	uttxrecordstatus text NULL,
-	uttxoutputresult text NULL,
 	uttxpaymentdate text NULL,
 	uttxpaymenttime text NULL,
 	uttxresponsecode text NULL,
@@ -89,7 +88,6 @@ CREATE TABLE tlpktc.paymentdetail (
 	uttxreferno2 text NULL,
 	uttxtrxntype text NULL,
 	uttxrecordstatus text NULL,
-	uttxoutputresult text NULL,
 	uttxpaymentdate text NULL,
 	uttxpaymenttime text NULL,
 	uttxresponsecode text NULL,
@@ -101,6 +99,324 @@ WITH (
 ) ;
 
 CREATE TABLE tlpktc.paymentfooter (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxtotdbamount text NULL,
+	uttxnodbitem text NULL,
+	uttxtotcramount text NULL,
+	uttxnocritem text NULL,
+	uttxtotrejdbamount text NULL,
+	uttxnorejdbitem text NULL,
+	uttxtotrejcramount text NULL,
+	uttxnorejcritem text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingheader1967 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxeffectivedate text NULL,
+	uttxfiledate text NULL,
+	uttxmerid text NULL,
+	uttxmername text NULL,
+	uttxcompacno text NULL,
+	uttxservicetype text NULL,
+	uttxbankcode text NULL,
+	uttxrecsize text NULL,
+	uttxfiletype text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingdetail1967 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxpan text NULL,
+	uttxcardholdername text NULL,
+	uttxtrxnamt text NULL,
+	uttxreferno1 text NULL,
+	uttxreferno2 text NULL,
+	uttxtrxntype text NULL,
+	uttxrecordstatus text NULL,
+	uttxpaymentdate text NULL,
+	uttxpaymenttime text NULL,
+	uttxresponsecode text NULL,
+	uttxauthcode text NULL,
+	expirydate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingfooter1967 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxtotdbamount text NULL,
+	uttxnodbitem text NULL,
+	uttxtotcramount text NULL,
+	uttxnocritem text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentheader1967 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxeffectivedate text NULL,
+	uttxfiledate text NULL,
+	uttxmerid text NULL,
+	uttxmername text NULL,
+	uttxcompacno text NULL,
+	uttxservicetype text NULL,
+	uttxbankcode text NULL,
+	uttxrecsize text NULL,
+	uttxfiletype text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentdetail1967 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxpan text NULL,
+	uttxcardholdername text NULL,
+	uttxtrxnamt text NULL,
+	uttxreferno1 text NULL,
+	uttxreferno2 text NULL,
+	uttxtrxntype text NULL,
+	uttxrecordstatus text NULL,
+	uttxpaymentdate text NULL,
+	uttxpaymenttime text NULL,
+	uttxresponsecode text NULL,
+	uttxauthcode text NULL,
+	expirydate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentfooter1967 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxtotdbamount text NULL,
+	uttxnodbitem text NULL,
+	uttxtotcramount text NULL,
+	uttxnocritem text NULL,
+	uttxtotrejdbamount text NULL,
+	uttxnorejdbitem text NULL,
+	uttxtotrejcramount text NULL,
+	uttxnorejcritem text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingheader1968 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxeffectivedate text NULL,
+	uttxfiledate text NULL,
+	uttxmerid text NULL,
+	uttxmername text NULL,
+	uttxcompacno text NULL,
+	uttxservicetype text NULL,
+	uttxbankcode text NULL,
+	uttxrecsize text NULL,
+	uttxfiletype text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingdetail1968 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxpan text NULL,
+	uttxcardholdername text NULL,
+	uttxtrxnamt text NULL,
+	uttxreferno1 text NULL,
+	uttxreferno2 text NULL,
+	uttxtrxntype text NULL,
+	uttxrecordstatus text NULL,
+	uttxpaymentdate text NULL,
+	uttxpaymenttime text NULL,
+	uttxresponsecode text NULL,
+	uttxauthcode text NULL,
+	expirydate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingfooter1968 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxtotdbamount text NULL,
+	uttxnodbitem text NULL,
+	uttxtotcramount text NULL,
+	uttxnocritem text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentheader1968 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxeffectivedate text NULL,
+	uttxfiledate text NULL,
+	uttxmerid text NULL,
+	uttxmername text NULL,
+	uttxcompacno text NULL,
+	uttxservicetype text NULL,
+	uttxbankcode text NULL,
+	uttxrecsize text NULL,
+	uttxfiletype text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentdetail1968 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxpan text NULL,
+	uttxcardholdername text NULL,
+	uttxtrxnamt text NULL,
+	uttxreferno1 text NULL,
+	uttxreferno2 text NULL,
+	uttxtrxntype text NULL,
+	uttxrecordstatus text NULL,
+	uttxpaymentdate text NULL,
+	uttxpaymenttime text NULL,
+	uttxresponsecode text NULL,
+	uttxauthcode text NULL,
+	expirydate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentfooter1968 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxtotdbamount text NULL,
+	uttxnodbitem text NULL,
+	uttxtotcramount text NULL,
+	uttxnocritem text NULL,
+	uttxtotrejdbamount text NULL,
+	uttxnorejdbitem text NULL,
+	uttxtotrejcramount text NULL,
+	uttxnorejcritem text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingheader1914 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxeffectivedate text NULL,
+	uttxfiledate text NULL,
+	uttxmerid text NULL,
+	uttxmername text NULL,
+	uttxcompacno text NULL,
+	uttxservicetype text NULL,
+	uttxbankcode text NULL,
+	uttxrecsize text NULL,
+	uttxfiletype text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingdetail1914 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxpan text NULL,
+	uttxcardholdername text NULL,
+	uttxtrxnamt text NULL,
+	uttxreferno1 text NULL,
+	uttxreferno2 text NULL,
+	uttxtrxntype text NULL,
+	uttxrecordstatus text NULL,
+	uttxpaymentdate text NULL,
+	uttxpaymenttime text NULL,
+	uttxresponsecode text NULL,
+	uttxauthcode text NULL,
+	expirydate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.billingfooter1914 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxtotdbamount text NULL,
+	uttxnodbitem text NULL,
+	uttxtotcramount text NULL,
+	uttxnocritem text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentheader1914 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxeffectivedate text NULL,
+	uttxfiledate text NULL,
+	uttxmerid text NULL,
+	uttxmername text NULL,
+	uttxcompacno text NULL,
+	uttxservicetype text NULL,
+	uttxbankcode text NULL,
+	uttxrecsize text NULL,
+	uttxfiletype text NULL,
+	filler text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentdetail1914 (
+	uttxrectype text NULL,
+	uttxseq text NULL,
+	uttxpan text NULL,
+	uttxcardholdername text NULL,
+	uttxtrxnamt text NULL,
+	uttxreferno1 text NULL,
+	uttxreferno2 text NULL,
+	uttxtrxntype text NULL,
+	uttxrecordstatus text NULL,
+	uttxpaymentdate text NULL,
+	uttxpaymenttime text NULL,
+	uttxresponsecode text NULL,
+	uttxauthcode text NULL,
+	expirydate text NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+CREATE TABLE tlpktc.paymentfooter1914 (
 	uttxrectype text NULL,
 	uttxseq text NULL,
 	uttxtotdbamount text NULL,
